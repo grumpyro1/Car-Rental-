f8278e932c596f12eac148fc72c4dc87a4cdeb3e
roanmilestanduyan <roanmilestanduyan@gmail.com>
2025-04-05 20:23:18 +0800

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter sds Home Page'),
     );
   }
 }
